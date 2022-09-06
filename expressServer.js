bbaca13478f039ef5d77e7844177231c334c3717
Jarret Coyle <jarretcoyletoo@gmail.com>
2022-09-05 20:25:56 -0700

    a bunch of messy code

--- expressServer.js
+++ expressServer.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -9,6 +11,28 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("*", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(port, () => {

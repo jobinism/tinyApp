fcd1122d3a03126736d3e8dc1843c22de090f5c6
Jarret Coyle <jarretcoyletoo@gmail.com>
2022-09-06 15:56:12 -0700

    Working through yesterday's additions, fixed priorities on /url/new

--- expressServer.js
+++ expressServer.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 8080; // default port 8080
+const port = 3005; // default port 8080
+const morgan = require('morgan');
 
 app.set("view engine", "ejs");
 
@@ -9,8 +10,14 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//Getterz ------------------------------------------------------
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls.json", (req, res) => {
@@ -21,23 +28,19 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("*", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
+
+
+//Userz ------------------------------------------------------------
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -46,8 +49,13 @@ app.post("/urls", (req, res) => {
   res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
-function generateRandomString() {}
+function generateRandomString() {
+  let value = Math.random().toString(36).substring(2, 7);
+  console.log("Value: ", value);
+  return value;
+}
+generateRandomString();
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`App listening on port ${port}!`);
 });
